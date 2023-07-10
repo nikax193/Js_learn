ff2f3bc48255997da58d6ba443c9ea9be62d86f4
Nika <nikax193@gmail.com>
2023-07-10 11:05:14 +0400

    cycles

--- js/script.js
+++ js/script.js
@@ -17,3 +17,87 @@ const storeDescription = {
 }
 
 console.log(storeDescription.open)
+
+//cycles
+
+let num = 50;
+
+// while (num < 55) {
+//     console.log(num);
+//     num++;
+// }
+// do {
+//     console.log(num);
+//     num++;
+// }
+// while (num < 55);
+
+for (let i = 1; i < 10; i++) {
+    console.log(num)
+    num++;
+}
+
+for (let i = 1; i < 10; i++) {
+    if (i === 8){
+        // break
+        continue
+    }
+    console.log(i)
+}
+
+//Цикл в цикле и метки
+for (let i = 0; i < 3; i++) {
+    console.log(i)
+    for (let j = 0; j < 3; j++) {
+        console.log(j)
+    }
+}
+
+// Asterix
+// *
+// **
+// ***
+// ****
+// *****
+// ******
+
+let result = '';
+const length = 7;
+
+for (let i = 1; i < length; i++) {
+    for (let j = 0; j < i; j++) {
+        result += "*"
+    }
+
+
+    result += '\n';
+}
+
+console.log(result) 
+
+//метки
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`)
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`)
+
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) continue first;
+            console.log(`Hhird level: ${k}`)
+
+        }
+    }
+}
+
+first: for (let i = 0; i < 3; i++) {
+    console.log(`First level: ${i}`)
+    for (let j = 0; j < 3; j++) {
+        console.log(`Second level: ${j}`)
+
+        for (let k = 0; k < 3; k++) {
+            if (k === 2) break first;
+            console.log(`Hhird level: ${k}`)
+
+        }
+    }
+}
